ab8af906ab2ffea815c956db03270f340f10fd30
y-du <42994541+y-du@users.noreply.github.com>
2018-02-28 15:28:34 +0100

    added serial version

---
+++ serial-version/ferraris-arduino-sensor-serial.ino
@@ -0,0 +1,182 @@
+const int sen_pin = A0;
+const int ir_pin = 6;
+const int ext_led_pin = 3;
+
+int new_avg_threshold = 4000;
+int detection_threshold = 100;
+int lower_limit_distance = 7;
+
+
+void blinkLED(const int *led_pin, int times=1, int pause=200) {
+  for (int bl = 0; bl < times; bl++) {
+    digitalWrite(*led_pin, HIGH);
+    delay(pause);
+    digitalWrite(*led_pin, LOW);
+    if (times > 1 and bl != times-1) {
+      delay(pause);
+    }
+  }
+}
+
+
+int denoisedRead(const int *sen_pin, const int *ir_pin, int pause=2000) {
+  int reading_ir, reading_no_ir;
+  digitalWrite(*ir_pin, HIGH);
+  delayMicroseconds(pause);
+  reading_ir = 1023 - analogRead(*sen_pin);
+  digitalWrite(*ir_pin, LOW);
+  delayMicroseconds(pause);
+  reading_no_ir = 1023 - analogRead(*sen_pin);
+  // debug line
+  //Serial.print(String(reading_ir) + " - " + String(reading_no_ir) + " = ");
+  return reading_ir - reading_no_ir;
+}
+
+
+String readLine(int timeout = 3000) {
+  String line = "";
+  bool endline = false;
+  int i_count = 10;
+  int interval = timeout / i_count;
+  if (Serial.available() > 0) {
+    while (endline == false) {
+      if (Serial.available() > 0) {
+        char recieved = Serial.read();
+        if (recieved == '\n') {
+          endline = true;
+        } else {
+          line += recieved;
+        }
+      } else {
+        if (i_count < 1) {
+          line = "";
+          break;
+        }
+        delay(interval);
+        i_count--;
+      }
+    }
+    return line;
+  }
+}
+
+
+void setup() {
+  Serial.begin(9600);
+  // wait for serial port to connect. Needed for native USB port only
+  while (!Serial) {
+    delay(1);
+  }
+
+  // set pins
+  pinMode(ir_pin, OUTPUT);
+  pinMode(ext_led_pin, OUTPUT);
+
+  // print start message
+  delay(2000);
+  Serial.println(F("RDY"));
+}
+
+
+bool detected = false;
+long reading_sum = 0;
+long current_avg = 0;
+long tmp_avg = 0;
+long iteration = 1;
+int new_avg_threshold_count = 1;
+bool calibrated = false;
+int detection_threshold_count = 1;
+String command = "";
+
+void getCommand() {
+  String line = readLine();
+  if (line != "") {
+    command = line;
+  }
+}
+
+void loop() {
+  getCommand();
+  
+  if (command == "CONF") {
+    Serial.println(F("NAT:DT:LLD"));
+    int i_count = 10;
+    int interval = 5000 / i_count;
+    while (command == "CONF") {
+      String conf_line = readLine();
+      if (conf_line != "") {
+        int pos = conf_line.indexOf(":");
+        new_avg_threshold = conf_line.substring(0, pos).toInt();
+        int pos2 = conf_line.indexOf(":", pos+1);
+        detection_threshold = conf_line.substring(pos+1, pos2).toInt();
+        lower_limit_distance = conf_line.substring(pos2+1).toInt();
+        Serial.println(String(new_avg_threshold) + ":" + String(detection_threshold) + ":" + String(lower_limit_distance));
+        command = "";
+      } else {
+        if (i_count < 1) {
+          Serial.println(String(new_avg_threshold) + ":" + String(detection_threshold) + ":" + String(lower_limit_distance));
+          command = "";
+          break;
+        }
+        delay(interval);
+        i_count--;
+      }
+    }
+    Serial.println(F("RDY"));
+  }
+  
+  if (command == "MR") {
+    while (command != "STP") {
+      getCommand();
+      Serial.println(analogRead(A0));
+      delay(100);
+    }
+    Serial.println(F("RDY"));
+  }
+  
+  if (command == "STRT") {
+    while (command != "STP") {
+      int reading = denoisedRead(&sen_pin, &ir_pin, 1800);
+      if (calibrated == true) {
+        if (reading < (current_avg - lower_limit_distance)) {
+          if (detection_threshold_count <= detection_threshold) {
+            detection_threshold_count++;
+          }
+          if (detection_threshold_count == detection_threshold) {
+            if (detected == false) {
+              detected = true;
+              Serial.println(F("DET"));
+            }
+          }
+        } else {
+          detection_threshold_count = 1;
+          if (detected == true) {
+            detected = false;
+          }
+        }
+      }
+      reading_sum = reading_sum + reading;
+      long average = reading_sum / iteration;
+      if (average == tmp_avg) {
+        new_avg_threshold_count++;
+      } else {
+        new_avg_threshold_count = 0;
+      }
+      if (new_avg_threshold_count == new_avg_threshold) {
+        current_avg = tmp_avg;
+        reading_sum = 0;
+        iteration = 1;
+        if (calibrated == false) {
+          calibrated = true;
+          blinkLED(&ext_led_pin);
+        }
+      }
+      tmp_avg = average;
+      iteration++;
+      delay(1);
+      getCommand();
+    }
+    Serial.println(F("RDY"));
+  }
+  delay(10);
+}
